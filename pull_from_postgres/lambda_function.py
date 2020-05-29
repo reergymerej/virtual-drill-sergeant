4babadacf10c54e8e307e72f759e314bf2665708
Jeremy Greer <jex.grizzle@gmail.com>
2020-05-28 21:53:45 -0700

    Refactor

--- pull_from_postgres/lambda_function.py
+++ pull_from_postgres/lambda_function.py
@@ -1,45 +1,51 @@
-import sys
 import logging
-import rds_config
 import psycopg2
-
-
-#rds settings
-rds_host  = "virtual-drill-sergeant.cl2pih0c6gge.us-east-1.rds.amazonaws.com"
-db_user = rds_config.db_username
-password = rds_config.db_password
-database = rds_config.db_name
-
+import rds_config
+import sys
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-try:
-    connection = psycopg2.connect(
-        host = rds_host,
-        port = 5432,
-        user = db_user,
-        password = password,
-        database = database
-    )
-except:
-    e = sys.exc_info()[0]
-    logger.error("ERROR: Unexpected error: Could not connect to db instance.")
-    logger.error(e)
-    sys.exit()
 
-logger.info("SUCCESS: Connection to RDS instance succeeded")
+def get_connection():
+    """
+    Create db connection
+    """
+
+    rds_host  = "virtual-drill-sergeant.cl2pih0c6gge.us-east-1.rds.amazonaws.com"
+    db_user = rds_config.db_username
+    password = rds_config.db_password
+    database = rds_config.db_name
+
+    try:
+        connection = psycopg2.connect(
+            host = rds_host,
+            port = 5432,
+            user = db_user,
+            password = password,
+            database = database
+        )
+    except:
+        e = sys.exc_info()[0]
+        logger.error("ERROR: Unexpected error: Could not connect to db instance.")
+        logger.error(e)
+        sys.exit()
+
+    logger.info("SUCCESS: Connection to RDS instance succeeded")
+    return connection
+
 
 def handler(event, context):
     """
     This function fetches content from MySQL RDS instance
     """
 
-    item_count = 0
+    connection = get_connection()
 
     with connection.cursor() as cur:
         print("Getting numbers...")
         cur.execute("select * from numbers")
+        item_count = 0
         for row in cur:
             item_count += 1
             logger.info(row)
