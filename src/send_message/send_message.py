0affc15b5599b51d88e2821d3cee11b8d5f59c24
Jeremy Greer <jex.grizzle@gmail.com>
2020-05-29 23:03:06 -0700

    Refactor

--- src/send_message/send_message.py
+++ src/send_message/send_message.py
@@ -3,16 +3,23 @@ import os
 import random
 from connect import get_connection
 
-def get_message():
-    commands = [
-        "30 second plank, go!",
-        "Do 10 diamond pushups!",
-        "Do 10 pushups!",
-        "Do 20 4-count flutter kicks!",
-        "Do 20 crunches!",
-        "Do 40 4-count side-straddle hops!",
-    ]
-    return random.choice(commands)
+def query(connection, sql):
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    return cursor
+
+def one(connection, sql):
+    return query(connection, sql).fetchone()
+
+def all(connection, sql):
+    return query(connection, sql).fetchall()
+
+def get_message(connection):
+    commands = all(connection, "SELECT text FROM commands")
+    return random.choice(commands)[0]
+
+def get_number(connection):
+    return one(connection, "SELECT phone FROM numbers")[0]
 
 def send_message(number, message):
     print('Send "{0}" to {1}.'.format(message, number))
@@ -26,19 +33,11 @@ def send_message(number, message):
         Message=message
     )
 
-def get_number(connection):
-    cursor = connection.cursor()
-    cursor.execute("SELECT phone FROM numbers")
-    top = cursor.fetchone()
-    phone = top[0]
-    return phone
-
 def lambda_handler(event, context):
     connection = get_connection()
     number = get_number(connection)
-    message = get_message()
+    message = get_message(connection)
     send_message(number, message)
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name="virtual-drill-sergeant")
     lambda_handler(None, None)
