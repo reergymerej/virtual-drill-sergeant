9866b54f0bdf78818cce49c93e63397b60302e35
Jeremy Greer <jex.grizzle@gmail.com>
2020-05-31 10:01:43 -0700

    Return message id

--- src/lambda/send_message/send_message.py
+++ src/lambda/send_message/send_message.py
@@ -3,7 +3,7 @@ import db
 import os
 
 def get_message():
-    return db.random("SELECT text FROM commands")[0]
+    return db.random("SELECT id, text FROM commands")
 
 def get_number():
     return db.one("SELECT phone FROM numbers")[0]
@@ -22,8 +22,13 @@ def send_message(number, message):
 
 def lambda_handler(event, context):
     number = get_number()
-    message = get_message()
-    send_message(number, message)
+    message_row = get_message()
+    message_id = message_row[0]
+    message_text = message_row[1]
+    send_message(number, message_text)
+    return {
+        "message": message_id,
+    }
 
 if __name__ == '__main__':
     lambda_handler(None, None)
