c019d81d17e70104c750401b4c8e5018ff1a5e64
Jeremy Greer <jex.grizzle@gmail.com>
2020-05-31 09:30:45 -0700

    Handle fk error

--- src/lambda/add_record/lambda_function.py
+++ src/lambda/add_record/lambda_function.py
@@ -1,6 +1,7 @@
 import boto3
 import db
 import os
+import psycopg2
 
 def get_number():
     return db.one("SELECT phone FROM numbers")[0]
@@ -21,9 +22,12 @@ def lambda_handler(event, context):
     command_id = get_command_id_from_event(event)
     try:
         insert_command(command_id)
+    except psycopg2.errors.ForeignKeyViolation as e:
+        print("The command_id {0} is not valid.".format(command_id))
     except Exception as e:
         print("There was an error inserting the record.")
         print(e)
+        raise e
 
 if __name__ == '__main__':
     lambda_handler(None, None)
