744667da0410cc1df6bbd5720a9fcebe7f47d0f5
Jeremy Greer <jex.grizzle@gmail.com>
2020-06-05 07:27:20 -0700

    Add phone number ref to logs

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ in shape?
 
 ## Wishlist
 
-
 * turn notifications on/off by phone number
+
 * segment log by days
 * share db across lambda functions to run locally
 * Use CloudFormation to create a stack for this.  This will help me learn AWS
--- src/lambda/logs/add_log.py
+++ src/lambda/logs/add_log.py
@@ -6,8 +6,8 @@ import psycopg2
 def insert_command(command_id, number_id):
     query = "INSERT INTO command_log(command_id, number_id) VALUES ({0}, {1})".format(command_id, number_id)
     print(query)
-    if os.getenv('DEV'):
-        return
+    # if os.getenv('DEV'):
+        # return
 
     return db.insert(query)
 
@@ -15,9 +15,13 @@ def get_command_id_from_event(event):
     responsePayload = event["responsePayload"]
     return responsePayload["message"]
 
+def get_phone_id_from_event(event):
+    responsePayload = event["responsePayload"]
+    return responsePayload.get("phone", 1)
+
 def lambda_handler(event, context):
     command_id = get_command_id_from_event(event)
-    number_id = 1
+    number_id = get_phone_id_from_event(event)
     try:
         insert_command(command_id, number_id)
     except psycopg2.errors.ForeignKeyViolation as e:
