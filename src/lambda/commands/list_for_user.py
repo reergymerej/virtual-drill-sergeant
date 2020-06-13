5cc1408cffe62ed3e1a488f75efda263d909bfdb
Jeremy Greer <jex.grizzle@gmail.com>
2020-06-13 12:19:37 -0700

    Create new lambda to get commands by user

---
+++ src/lambda/commands/list_for_user.py
@@ -0,0 +1,42 @@
+import json
+import db
+import os
+
+def list_command(user_id):
+    query = """
+        select user_commands.id
+        ,commands.text
+        ,user_commands.disabled
+        from user_commands
+        join commands on commands.id = user_commands.command_id
+        where user_commands.user_id = {user_id}
+    """.format(user_id=user_id)
+    print(query)
+    if os.getenv('DEV'):
+        return
+    return db.all(query)
+
+def get_response(body):
+    return {
+        "isBase64Encoded": "false",
+        "statusCode": 200, # 204 won't send body to api gateway for some reason
+        "headers": {
+            "Access-Control-Allow-Origin":"*",
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(body),
+    }
+
+def get_phone_from_event(event):
+    return int(event.get("pathParameters", {}).get("phone", 1))
+
+def lambda_handler(event, context):
+    user_id = get_phone_from_event(event)
+    result = list_command(user_id)
+    print(result)
+    return get_response(result)
+
+if __name__ == '__main__':
+    with open('./list-for-user-event.json') as f:
+        event = json.load(f)
+        lambda_handler(event, None)
